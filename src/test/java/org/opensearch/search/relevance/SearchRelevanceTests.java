8ae48d387f80c323ce58933b440926d96e7e3412
🍕Kevin Li <kevlim@amazon.com>
2022-07-11 11:26:13 -0400

    Initial code for plugin based on opensearch-plugin-template-java
    Signed-off-by: 🍕Kevin Li <kevlim@amazon.com>

---
+++ src/test/java/org/opensearch/search/relevance/SearchRelevanceTests.java
@@ -0,0 +1,14 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+package org.opensearch.search.relevance;
+
+import org.opensearch.test.OpenSearchTestCase;
+
+public class SearchRelevanceTests extends OpenSearchTestCase {
+    // Add unit tests for your plugin
+}
