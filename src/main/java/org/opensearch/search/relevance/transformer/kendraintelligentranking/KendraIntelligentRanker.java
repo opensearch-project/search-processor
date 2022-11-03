c6c9c1220bfaced172bc55853947b1c1c9ac3206
Mahita Mahesh <mahitam@amazon.com>
2022-11-02 22:29:57 -0400

    Preprocess search request to send more search hits to Kendra Ranking than the default 10.
    * Add preprocess request interface to ResultTransformer
    * Override from and size parameters durig preprocess, to fetch more hits from OpenSearch
    * Limit the number of documents sent to Kendra Ranking (Will make this number configurable in a follow up)
    * Populate response documents from re-ranked hits (upto Kendra limit) and OpenSearch hits beyond the limit
    
    Signed-off-by: Mahita Mahesh <mahitam@amazon.com>
    Signed-off-by: Michael Froh <froh@amazon.com>

--- src/main/java/org/opensearch/search/relevance/transformer/kendraintelligentranking/KendraIntelligentRanker.java
+++ src/main/java/org/opensearch/search/relevance/transformer/kendraintelligentranking/KendraIntelligentRanker.java
@@ -7,14 +7,21 @@
  */
 package org.opensearch.search.relevance.transformer.kendraintelligentranking;
 
+import static org.opensearch.search.relevance.transformer.kendraintelligentranking.configuration.Constants.BODY_FIELD;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.PriorityQueue;
 
+import java.util.Set;
+import java.util.stream.Collectors;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -22,10 +29,12 @@ import org.opensearch.action.search.SearchRequest;
 import org.opensearch.common.settings.Setting;
 import org.opensearch.search.SearchHit;
 import org.opensearch.search.SearchHits;
+import org.opensearch.search.SearchService;
 import org.opensearch.search.relevance.configuration.ResultTransformerConfiguration;
 import org.opensearch.search.relevance.transformer.ResultTransformer;
 import org.opensearch.search.relevance.transformer.kendraintelligentranking.client.KendraHttpClient;
 import org.opensearch.search.relevance.transformer.kendraintelligentranking.configuration.KendraIntelligentRankingConfiguration;
+import org.opensearch.search.relevance.transformer.kendraintelligentranking.model.KendraIntelligentRankingException;
 import org.opensearch.search.relevance.transformer.kendraintelligentranking.model.PassageScore;
 import org.opensearch.search.relevance.transformer.kendraintelligentranking.model.dto.Document;
 import org.opensearch.search.relevance.transformer.kendraintelligentranking.model.dto.RescoreRequest;
@@ -46,6 +55,7 @@ public class KendraIntelligentRanker implements ResultTransformer {
   private static final double BM25_B_VALUE = 0.75;
   private static final double BM25_K1_VALUE = 1.6;
   private static final int TOP_K_PASSAGES = 3;
+  private static final int KENDRA_DOC_LIMIT = 25;
 
   private static final Logger logger = LogManager.getLogger(KendraIntelligentRanker.class);
 
@@ -70,17 +80,40 @@ public class KendraIntelligentRanker implements ResultTransformer {
    * @return boolean decision on whether to re-rank
    */
   @Override
-  public boolean shouldTransform(SearchRequest request, ResultTransformerConfiguration configuration) {
+  public boolean shouldTransform(final SearchRequest request, final ResultTransformerConfiguration configuration) {
     if (request.source() == null) {
       return false;
     }
 
-    // Skip if there is scroll, pagination, or sorting.
-    if (request.scroll() != null || request.source().from() > 0 ||
-        (request.source().sorts() != null && !request.source().sorts().isEmpty())) {
+    // Skip if there is scroll, sorting, or the start of the page is greater than the document limit for Kendra Ranking
+    if (request.scroll() != null ||
+        (request.source().sorts() != null && !request.source().sorts().isEmpty()) ||
+        request.source().from() >= KENDRA_DOC_LIMIT) {
       return false;
     }
     return true;
+  }
+
+  @Override
+  public SearchRequest preprocessRequest(final SearchRequest request,
+      final SearchRequest originalSearchRequest,
+      final ResultTransformerConfiguration configuration) {
+    // Source is returned in response hits by default. If disabled by the user, overwrite and enable
+    // in order to access document contents for reranking, then suppress at response time.
+    if (request.source().fetchSource() != null && !request.source().fetchSource().fetchSource()) {
+      originalSearchRequest.source().fetchSource(request.source().fetchSource());
+      request.source().fetchSource(true);
+    }
+
+    int from = request.source().from() == -1 ? SearchService.DEFAULT_FROM : request.source().from();
+    int size = request.source().size() == -1 ? SearchService.DEFAULT_SIZE : request.source().size();
+    originalSearchRequest.source().from(from);
+    originalSearchRequest.source().size(size);
+
+    int sizeOverride = Math.max(KENDRA_DOC_LIMIT, from + size);
+    request.source().from(0);
+    request.source().size(sizeOverride);
+    return request;
   }
 
   /**
@@ -102,49 +135,66 @@ public class KendraIntelligentRanker implements ResultTransformer {
       return hits;
     }
     try {
-      List<Document> originalHits = new ArrayList<>();
-      for (SearchHit searchHit : hits.getHits()) {
-        Map<String, Object> docSourceMap = searchHit.getSourceAsMap();
+      List<SearchHit> originalHits = Arrays.asList(hits.getHits());
+      final int numberOfHitsToRerank = Math.min(originalHits.size(), KENDRA_DOC_LIMIT);
+      List<Document> originalHitsAsDocuments = new ArrayList<>();
+      Map<String, SearchHit> idToSearchHitMap = new HashMap<>();
+      for (int j = 0; j < numberOfHitsToRerank; ++j) {
+        Map<String, Object> docSourceMap = originalHits.get(j).getSourceAsMap();
         SlidingWindowTextSplitter textSplitter = new SlidingWindowTextSplitter(PASSAGE_SIZE_LIMIT, SLIDING_WINDOW_STEP, MAXIMUM_PASSAGES);
-        List<String> splitPassages = textSplitter.split(docSourceMap.get(queryParserResult.getBodyFieldName()).toString());
+        String bodyFieldName = queryParserResult.getBodyFieldName();
+        String titleFieldName = queryParserResult.getTitleFieldName();
+        if (docSourceMap.get(bodyFieldName) == null) {
+          String errorMessage = String.format(Locale.ENGLISH,
+              "Kendra Intelligent Ranking cannot be applied when documents are missing %s [%s]. Document ID [].",
+              BODY_FIELD, bodyFieldName, originalHits.get(j).getId());
+          logger.error(errorMessage);
+          throw new KendraIntelligentRankingException(errorMessage);
+        }
+        List<String> splitPassages = textSplitter.split(docSourceMap.get(bodyFieldName).toString());
         List<List<String>> topPassages = getTopPassages(queryParserResult.getQueryText(), splitPassages);
         List<String> tokenizedTitle = null;
-        if (queryParserResult.getTitleFieldName() != null) {
+        if (titleFieldName != null && docSourceMap.get(titleFieldName) != null) {
           tokenizedTitle = textTokenizer.tokenize(docSourceMap.get(queryParserResult.getTitleFieldName()).toString());
           // If tokens list is empty, use null
           if (tokenizedTitle.isEmpty()) {
             tokenizedTitle = null;
           }
         }
-        for (int i = 0; i < topPassages.size(); i++) {
-          originalHits.add(
-              new Document(searchHit.getId() + "@" + (i + 1), searchHit.getId(), tokenizedTitle, topPassages.get(i), searchHit.getScore())
+        for (int i = 0; i < topPassages.size(); ++i) {
+          originalHitsAsDocuments.add(
+              new Document(originalHits.get(j).getId() + "@" + (i + 1), originalHits.get(j).getId(), tokenizedTitle, topPassages.get(i), originalHits.get(j).getScore())
           );
         }
+        // Map search hits by their ID in order to map Kendra response documents back to hits later
+        idToSearchHitMap.put(originalHits.get(j).getId(), originalHits.get(j));
       }
 
-      final RescoreRequest rescoreRequest = new RescoreRequest(queryParserResult.getQueryText(), originalHits);
+      final RescoreRequest rescoreRequest = new RescoreRequest(queryParserResult.getQueryText(), originalHitsAsDocuments);
       final RescoreResult rescoreResult = kendraClient.rescore(rescoreRequest);
-      Map<String, SearchHit> idToSearchHitMap = new HashMap<>();
-      for (SearchHit searchHit : hits.getHits()) {
-        idToSearchHitMap.put(searchHit.getId(), searchHit);
-      }
+
       List<SearchHit> newSearchHits = new ArrayList<>();
       float maxScore = 0;
       for (RescoreResultItem rescoreResultItem : rescoreResult.getResultItems()) {
         SearchHit searchHit = idToSearchHitMap.get(rescoreResultItem.getDocumentId());
         if (searchHit == null) {
-          logger.warn("Response from external service references hit id {}, which does not exist in original results. Skipping.",
+          String errorMessage = String.format(Locale.ENGLISH,
+              "Response from Kendra Intelligent Ranking service references document ID [%s], which does not exist in original results",
               rescoreResultItem.getDocumentId());
-          continue;
+          logger.error(errorMessage);
+          throw new KendraIntelligentRankingException(errorMessage);
         }
         searchHit.score(rescoreResultItem.getScore());
         maxScore = Math.max(maxScore, rescoreResultItem.getScore());
         newSearchHits.add(searchHit);
       }
+      // Add remaining hits to response, which are already sorted by OpenSearch score
+      for (int i = KENDRA_DOC_LIMIT; i < originalHits.size(); ++i) {
+        newSearchHits.add(originalHits.get(i));
+      }
       return new SearchHits(newSearchHits.toArray(new SearchHit[newSearchHits.size()]), hits.getTotalHits(), maxScore);
     } catch (Exception ex) {
-      logger.error("Failed to re-rank. Returning original search results without re-ranking.", ex);
+      logger.error("Failed to rescore. Returning original search results without rescore.", ex);
       return hits;
     }
   }
