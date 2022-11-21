64926c5f5a4dbd5cdf206048d6d22b26f4de9aaf
Mark Cohen <macohen@users.noreply.github.com>
2022-11-21 16:49:31 -0500

    Enabling Windows Build and Jacoco Reporting (#28)
    * adding codecov and windows build commands
    * output jacoco test reports
    * enabling jacoco test report output in build.gradle
    * adding dependabot check at a daily interval
    * added test that adds 1 more line of coverage. :|
    * fixing dependabot configurations
    * fixing tests - adding 1 line coverage - woohoo
    * removed spotlessCheck
    
    Signed-off-by: Mark Cohen <markcoh@amazon.com>

---
+++ src/test/java/org/opensearch/search/relevance/transformer/kendraintelligentranking/configuration/KendraIntelligentRankingConfigurationTests.java
@@ -0,0 +1,23 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+package org.opensearch.search.relevance.transformer.kendraintelligentranking.configuration;
+
+import org.junit.Test;
+import org.opensearch.test.OpenSearchTestCase;
+
+import java.io.IOException;
+
+public class KendraIntelligentRankingConfigurationTests extends OpenSearchTestCase {
+    public void testParseWithNullParserAndContext() {
+        try {
+            KendraIntelligentRankingConfiguration.parse(null, null);
+            fail();
+        } catch (NullPointerException | IOException e) {
+        }
+    }
+}
