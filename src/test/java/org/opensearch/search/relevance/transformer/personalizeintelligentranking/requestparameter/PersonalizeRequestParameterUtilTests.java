1387a10b455679a0dd99eb6126a2b242357e3cf5
Ketan Kulkarni <kektnr@amazon.com>
2023-05-26 12:09:27 -0700

    Improve unit test coverage
    Description:
    ============
    Change adds more unit test coverage for the context parameters equality check
    
    Signed-off-by: Ketan Kulkarni <kektnr@amazon.com>

--- src/test/java/org/opensearch/search/relevance/transformer/personalizeintelligentranking/requestparameter/PersonalizeRequestParameterUtilTests.java
+++ src/test/java/org/opensearch/search/relevance/transformer/personalizeintelligentranking/requestparameter/PersonalizeRequestParameterUtilTests.java
@@ -59,4 +59,39 @@ public class PersonalizeRequestParameterUtilTests extends OpenSearchTestCase {
         PersonalizeRequestParameters actual = PersonalizeRequestParameterUtil.getPersonalizeRequestParameters(request);
         assertNotEquals(notExpected, actual);
     }
+
+    public void testPersonalizeRequestParametersContextMapDifferentSize() {
+        Map<String, Object> notExpectedContext = new HashMap<>();
+        notExpectedContext.put("contextKey", "contextValue");
+        PersonalizeRequestParameters notExpected = new PersonalizeRequestParameters("user_1", notExpectedContext);
+
+        Map<String, Object> expectedContext = new HashMap<>();
+        expectedContext.put("contextKey2", "contextValue2");
+        expectedContext.put("contextKey22", "contextValue22");
+        PersonalizeRequestParameters expected = new PersonalizeRequestParameters("user_1", expectedContext);
+        PersonalizeRequestParametersExtBuilder extBuilder = new PersonalizeRequestParametersExtBuilder();
+        extBuilder.setRequestParameters(expected);
+        SearchSourceBuilder sourceBuilder = SearchSourceBuilder.searchSource()
+                .ext(List.of(extBuilder));
+        SearchRequest request = new SearchRequest("my_index").source(sourceBuilder);
+        PersonalizeRequestParameters actual = PersonalizeRequestParameterUtil.getPersonalizeRequestParameters(request);
+        assertNotEquals(notExpected, actual);
+    }
+
+    public void testPersonalizeRequestParametersUserIdDiffers() {
+        Map<String, Object> notExpectedContext = new HashMap<>();
+        notExpectedContext.put("contextKey", "contextValue");
+        PersonalizeRequestParameters notExpected = new PersonalizeRequestParameters("user_1", notExpectedContext);
+
+        Map<String, Object> expectedContext = new HashMap<>();
+        expectedContext.put("contextKey", "contextValue");
+        PersonalizeRequestParameters expected = new PersonalizeRequestParameters("user_2", expectedContext);
+        PersonalizeRequestParametersExtBuilder extBuilder = new PersonalizeRequestParametersExtBuilder();
+        extBuilder.setRequestParameters(expected);
+        SearchSourceBuilder sourceBuilder = SearchSourceBuilder.searchSource()
+                .ext(List.of(extBuilder));
+        SearchRequest request = new SearchRequest("my_index").source(sourceBuilder);
+        PersonalizeRequestParameters actual = PersonalizeRequestParameterUtil.getPersonalizeRequestParameters(request);
+        assertNotEquals(notExpected, actual);
+    }
 }
