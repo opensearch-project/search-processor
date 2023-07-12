c12b4290d6d5c3d6a8eac5d739e540908a74f9cf
kulket <130191298+kulket@users.noreply.github.com>
2023-07-11 18:49:33 -0700

    Add validations for Personalize input and configurations (#151)
    * Add validations for Personalize input and configurations
    
    Description:
    ===========
    This commit adds following changes:
    * Change scoring logic to consider ranks instead of scores
    * Add validation for Personalize inputs and configurations
    * Add E2E tests in the form of unit tests for Personalize ranking
    
    Signed-off-by: Ketan Kulkarni <kektnr@amazon.com>
    
    * Add user agent prefix to Personalize client to understand plugin adoption
    
    Description:
    ============
    This change adds following:
    * User agent configuration for Personalize client to understand plugin adoption
    * Add validation related unit tests
    
    Signed-off-by: Ketan Kulkarni <kektnr@amazon.com>
    
    * Add validations for Personalize input and configurations
    
    Description:
    ===========
    This commit adds following changes:
    * Change scoring logic to consider ranks instead of scores
    * Add validation for Personalize inputs and configurations
    * Add E2E tests in the form of unit tests for Personalize ranking
    
    Signed-off-by: Ketan Kulkarni <kektnr@amazon.com>
    
    * Change response processor config name
    
    Description:
    ===========
    This commit includes following:
    * Change response processor name from personalize_ranking to personalized_search_ranking
    * Change client settings to use same naming convention
    * Address review comments
    
    Signed-off-by: Ketan Kulkarni <kektnr@amazon.com>
    
    * Fix typo in unit test
    
    Signed-off-by: Ketan Kulkarni <kektnr@amazon.com>
    
    ---------
    
    Signed-off-by: Ketan Kulkarni <kektnr@amazon.com>
    (cherry picked from commit e305c138d34253d57d24486fc30d1238fa867a1b)

---
+++ src/main/java/org/opensearch/search/relevance/transformer/personalizeintelligentranking/utils/ValidationUtil.java
@@ -0,0 +1,60 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+package org.opensearch.search.relevance.transformer.personalizeintelligentranking.utils;
+import com.amazonaws.arn.Arn;
+import org.opensearch.ingest.ConfigurationUtils;
+import org.opensearch.search.relevance.transformer.personalizeintelligentranking.configuration.PersonalizeIntelligentRankerConfiguration;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.HashSet;
+
+import static org.opensearch.search.relevance.transformer.personalizeintelligentranking.configuration.Constants.AMAZON_PERSONALIZED_RANKING_RECIPE_NAME;
+
+public class ValidationUtil {
+    private static Set<String> SUPPORTED_PERSONALIZE_RECIPES = new HashSet<>(Arrays.asList(AMAZON_PERSONALIZED_RANKING_RECIPE_NAME));
+
+    /**
+     * Validate Personalize configuration for calling Personalize service.
+     * Throws OpenSearchParseException type exception if validation fails.
+     * @param config Personalize intelligent ranker configuration
+     * @param processorType Name of search pipeline processor
+     * @param processorTag Name of processor tag
+     */
+    public static void validatePersonalizeIntelligentRankerConfiguration (PersonalizeIntelligentRankerConfiguration config,
+                                                                          String processorType,
+                                                                          String processorTag
+                                                                          ) {
+        // Validate weight value
+        if (config.getWeight() < 0.0 || config.getWeight() > 1.0) {
+            throw ConfigurationUtils.newConfigurationException(processorType, processorTag, "weight", "invalid value for weight");
+        }
+        // Validate Personalize campaign ARN
+        if(!isValidCampaignOrRoleArn(config.getPersonalizeCampaign(), "personalize")) {
+            throw ConfigurationUtils.newConfigurationException(processorType, processorTag, "campaign_arn", "invalid format for Personalize campaign arn");
+        }
+        // Validate IAM Role Arn for Personalize access
+        String iamRoleArn = config.getIamRoleArn();
+        if(!(iamRoleArn == null || iamRoleArn.isBlank()) && !isValidCampaignOrRoleArn(iamRoleArn, "iam")) {
+            throw ConfigurationUtils.newConfigurationException(processorType, processorTag, "iam_role_arn", "invalid format for Personalize iam role arn");
+        }
+        // Validate Personalize recipe
+        if(!SUPPORTED_PERSONALIZE_RECIPES.contains(config.getRecipe())) {
+            throw ConfigurationUtils.newConfigurationException(processorType, processorTag, "recipe", "not supported recipe provided");
+        }
+    }
+
+    private static boolean isValidCampaignOrRoleArn(String arn, String expectedService) {
+        try {
+            Arn arnObj = Arn.fromString(arn);
+            return arnObj.getService().equals(expectedService);
+        } catch (IllegalArgumentException iae) {
+            return false;
+        }
+    }
+}
