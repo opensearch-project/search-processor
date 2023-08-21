693530ffd9d03cddf0acab5a6029021c61ee738c
Michael Froh <froh@amazon.com>
2023-08-21 23:34:17 +0000

    Fix backport to 2.x
    We need to specify the opensearch version in two places now (until we
    figure out how to set it in one place), and need to switch to Apache
    HttpClient 4 on the 2.x branch.
    
    Signed-off-by: Michael Froh <froh@amazon.com>

--- amazon-kendra-intelligent-ranking/src/test/java/org/opensearch/search/relevance/AmazonKendraIntelligentRankingPluginIT.java
+++ amazon-kendra-intelligent-ranking/src/test/java/org/opensearch/search/relevance/AmazonKendraIntelligentRankingPluginIT.java
@@ -7,8 +7,8 @@
  */
 package org.opensearch.search.relevance;
 
-import org.apache.hc.core5.http.ParseException;
-import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.apache.http.ParseException;
+import org.apache.http.util.EntityUtils;
 import org.opensearch.client.Request;
 import org.opensearch.client.Response;
 import org.opensearch.test.rest.OpenSearchRestTestCase;
