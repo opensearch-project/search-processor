c693311465312ffe8a610055870a621f496a6a02
Michael Froh <froh@amazon.com>
2023-08-22 11:11:30 -0700

    [Backport 2.9][Backport 2.x] Split repository into separate plugins (#192) (#193)
    * Split repository into separate plugins (#191)
    
    (cherry picked from commit eb77879e694ca609929b0c1098620a6ebea4afda)
    
    * Fix backport to 2.x
    
    We need to specify the opensearch version in two places now (until we
    figure out how to set it in one place), and need to switch to Apache
    HttpClient 4 on the 2.x branch.
    
    
    
    ---------
    
    
    
    (cherry picked from commit 182937d3ee4702a8368f68d42fcee3c4cc0ece92)
    
    Signed-off-by: Michael Froh <froh@amazon.com>
    Co-authored-by: opensearch-trigger-bot[bot] <98922864+opensearch-trigger-bot[bot]@users.noreply.github.com>

---
+++ amazon-personalize-ranking/src/main/java/org/opensearch/search/relevance/AmazonPersonalizeRankingPlugin.java
@@ -0,0 +1,80 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+package org.opensearch.search.relevance;
+
+import org.opensearch.client.Client;
+import org.opensearch.cluster.metadata.IndexNameExpressionResolver;
+import org.opensearch.cluster.service.ClusterService;
+import org.opensearch.common.io.stream.NamedWriteableRegistry;
+import org.opensearch.common.settings.Setting;
+import org.opensearch.core.xcontent.NamedXContentRegistry;
+import org.opensearch.env.Environment;
+import org.opensearch.env.NodeEnvironment;
+import org.opensearch.plugins.Plugin;
+import org.opensearch.plugins.SearchPipelinePlugin;
+import org.opensearch.plugins.SearchPlugin;
+import org.opensearch.repositories.RepositoriesService;
+import org.opensearch.script.ScriptService;
+import org.opensearch.search.pipeline.Processor;
+import org.opensearch.search.pipeline.SearchResponseProcessor;
+import org.opensearch.search.relevance.transformer.personalizeintelligentranking.PersonalizeRankingResponseProcessor;
+import org.opensearch.search.relevance.transformer.personalizeintelligentranking.client.PersonalizeClientSettings;
+import org.opensearch.search.relevance.transformer.personalizeintelligentranking.requestparameter.PersonalizeRequestParametersExtBuilder;
+import org.opensearch.threadpool.ThreadPool;
+import org.opensearch.watcher.ResourceWatcherService;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
+public class AmazonPersonalizeRankingPlugin extends Plugin implements SearchPlugin, SearchPipelinePlugin {
+
+    private PersonalizeClientSettings personalizeClientSettings;
+
+    @Override
+    public List<Setting<?>> getSettings() {
+        // Add settings for other transformers here
+        return new ArrayList<>(PersonalizeClientSettings.getAllSettings());
+    }
+
+    @Override
+    public Collection<Object> createComponents(
+            Client client,
+            ClusterService clusterService,
+            ThreadPool threadPool,
+            ResourceWatcherService resourceWatcherService,
+            ScriptService scriptService,
+            NamedXContentRegistry xContentRegistry,
+            Environment environment,
+            NodeEnvironment nodeEnvironment,
+            NamedWriteableRegistry namedWriteableRegistry,
+            IndexNameExpressionResolver indexNameExpressionResolver,
+            Supplier<RepositoriesService> repositoriesServiceSupplier
+    ) {
+       this.personalizeClientSettings = PersonalizeClientSettings.getClientSettings(environment.settings());
+
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<SearchPlugin.SearchExtSpec<?>> getSearchExts() {
+        return List.of(
+                new SearchPlugin.SearchExtSpec<>(PersonalizeRequestParametersExtBuilder.NAME,
+                        PersonalizeRequestParametersExtBuilder::new,
+                        PersonalizeRequestParametersExtBuilder::parse));
+    }
+
+    @Override
+    public Map<String, Processor.Factory<SearchResponseProcessor>> getResponseProcessors(Parameters parameters) {
+        return Map.of(PersonalizeRankingResponseProcessor.TYPE, new PersonalizeRankingResponseProcessor.Factory(this.personalizeClientSettings));
+    }
+}
